da079e5662f2c608823a185fd99d9ff4d432727d
Arunasasindran <103830341+Arunasasindran@users.noreply.github.com>
2022-04-19 15:54:07 +0530

    Update MissingElementInAnArray.java

--- MissingElementInAnArray.java
+++ MissingElementInAnArray.java
@@ -8,20 +8,27 @@ public class MissingElementInAnArray {
 		// TODO Auto-generated method stub
 
 		int[] arr = { 1, 2, 3, 4, 7, 6, 8 };
+
+		// length of array
 		int length = arr.length;
-		System.out.println(length);
+		System.out.println("The length of array is " + length);
+
+		// sorting array
 		Arrays.sort(arr);
+		for (int j = 0; j < length; j++) {
+			System.out.println("The Sorted array is " + arr[j]);
+		}
+		// missing element in the array
+		for (int i = 1; i <= length + 1; i++) {
 
-		for (int i = 1; i <=length+1; i++) {
-
-			if (i != arr[i-1]) {
-				System.out.println(i);
+			if (i != arr[i - 1]) {
+				System.out.println("The missing element in the array is " + i);
 				break;
-				}
-			
+			}
 
 		}
 
 	}
 
 }
+
